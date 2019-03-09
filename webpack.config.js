6e32126bb8fc8b5b610abff35cb9b2ce4da0e73c
Boris Berak <bberak@gmail.com>
2019-03-09 17:59:13 +1100

    Fixed build and bumped version to 0.0.2

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['env'],
+            plugins: ['transform-class-properties']
           }
         }
       }
